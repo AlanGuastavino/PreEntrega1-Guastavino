2243cf7c6b038aaffb2a5fab5b022e57744cb8d1
alan guastavino <alan.guastavino18@gmail.com>
2023-07-24 20:51:42 -0300

    funciones

--- main.js
+++ main.js
@@ -1,56 +1,124 @@
-//Primera entrega - Javascript - Alan Guastavino
+let marcaSeleccionada;
+do {
+  marcaSeleccionada = prompt("Elija una marca entre: Asus, Acer, LG");
+} while (marcaSeleccionada !== "Asus" && marcaSeleccionada !== "Acer" && marcaSeleccionada !== "LG");
 
-const modelosMonitores = [
-    ["Asus", "Monitor Gamer Pro", "IPS", "24 pulgadas", { "60Hz": 97899, "144Hz": 322650 }],
-    ["Asus", "Monitor Gaming Master", "TN", "27 pulgadas", { "60Hz": 96500, "144Hz": 350800 }],
-    ["Acer", "Monitor Nitro", "IPS", "27 pulgadas", { "60Hz": 82500, "144Hz": 235690 }],
-    ["Acer", "Monitor Predator", "VA", "32 pulgadas", { "60Hz": 150000, "144Hz": 785900 }],
-    ["LG", "Monitor UltraGear", "IPS", "24 pulgadas", { "60Hz": 280845, "144Hz": 290000 }],
-    ["LG", "Monitor Gaming Pro", "TN", "27 pulgadas", { "60Hz": 210360, "144Hz": 320980 }]
-  ];
-  
-  function calcularCostoMonitor(marca, modelo, tipoPanel, frecuencia) {
-    for (let i = 0; i < modelosMonitores.length; i++) {
-      const monitor = modelosMonitores[i];
-      if (monitor[0] === marca && monitor[1] === modelo && monitor[2] === tipoPanel) {
-        return monitor[4][frecuencia];
+let modeloSeleccionado;
+do {
+  modeloSeleccionado = prompt(`Elija un modelo entre: Monitor Gamer Pro, Monitor Gaming Master, Monitor Nitro, Monitor Predator, Monitor UltraGear, Monitor Gaming Pro`);
+} while (
+  (marcaSeleccionada === "Asus" && modeloSeleccionado !== "Monitor Gamer Pro" && modeloSeleccionado !== "Monitor Gaming Master") ||
+  (marcaSeleccionada === "Acer" && modeloSeleccionado !== "Monitor Nitro" && modeloSeleccionado !== "Monitor Predator") ||
+  (marcaSeleccionada === "LG" && modeloSeleccionado !== "Monitor UltraGear" && modeloSeleccionado !== "Monitor Gaming Pro")
+);
+
+let tipoPanelSeleccionado;
+do {
+  tipoPanelSeleccionado = prompt("Elija un tipo de panel entre: IPS, TN, VA");
+} while (tipoPanelSeleccionado !== "IPS" && tipoPanelSeleccionado !== "TN" && tipoPanelSeleccionado !== "VA");
+
+let pulgadasSeleccionadas;
+do {
+  pulgadasSeleccionadas = prompt("Elija una pulgada entre: 24 pulgadas, 27 pulgadas, 32 pulgadas");
+} while (pulgadasSeleccionadas !== "24 pulgadas" && pulgadasSeleccionadas !== "27 pulgadas" && pulgadasSeleccionadas !== "32 pulgadas");
+
+let frecuenciaSeleccionada;
+do {
+  frecuenciaSeleccionada = prompt("Elija una frecuencia entre: 60Hz, 144Hz");
+} while (frecuenciaSeleccionada !== "60Hz" && frecuenciaSeleccionada !== "144Hz");
+
+let costoMonitor = 0;
+
+if (marcaSeleccionada === "Asus") {
+  if (modeloSeleccionado === "Monitor Gamer Pro") {
+    if (tipoPanelSeleccionado === "IPS") {
+      if (pulgadasSeleccionadas === "24 pulgadas") {
+        if (frecuenciaSeleccionada === "60Hz") {
+          costoMonitor = 97899;
+        } else if (frecuenciaSeleccionada === "144Hz") {
+          costoMonitor = 322650;
+        }
+      } else if (pulgadasSeleccionadas === "27 pulgadas") {
+        if (frecuenciaSeleccionada === "60Hz") {
+          costoMonitor = 96500;
+        } else if (frecuenciaSeleccionada === "144Hz") {
+          costoMonitor = 350800;
+        }
+      }
+    } else if (tipoPanelSeleccionado === "TN") {
+      if (pulgadasSeleccionadas === "27 pulgadas") {
+        if (frecuenciaSeleccionada === "60Hz") {
+          costoMonitor = 212500;
+        } else if (frecuenciaSeleccionada === "144Hz") {
+          costoMonitor = 320900;
+        }
       }
     }
-    return 0; 
+  } else if (modeloSeleccionado === "Monitor Gaming Master") {
+    if (tipoPanelSeleccionado === "IPS") {
+      if (pulgadasSeleccionadas === "27 pulgadas") {
+        if (frecuenciaSeleccionada === "60Hz") {
+          costoMonitor = 82500;
+        } else if (frecuenciaSeleccionada === "144Hz") {
+          costoMonitor = 235690;
+        }
+      }
+    }
   }
-  function validarOpcion(opcion, opcionesValidas) {
-    return opcionesValidas.includes(opcion);
+} else if (marcaSeleccionada === "Acer") {
+  if (modeloSeleccionado === "Monitor Nitro") {
+    if (tipoPanelSeleccionado === "IPS") {
+      if (pulgadasSeleccionadas === "27 pulgadas") {
+        if (frecuenciaSeleccionada === "60Hz") {
+          costoMonitor = 82500;
+        } else if (frecuenciaSeleccionada === "144Hz") {
+          costoMonitor = 235690;
+        }
+      }
+    }
+  } else if (modeloSeleccionado === "Monitor Predator") {
+    if (tipoPanelSeleccionado === "VA") {
+      if (pulgadasSeleccionadas === "32 pulgadas") {
+        if (frecuenciaSeleccionada === "60Hz") {
+          costoMonitor = 150000;
+        } else if (frecuenciaSeleccionada === "144Hz") {
+          costoMonitor = 785900;
+        }
+      }
+    }
   }
-  
-  let marcaSeleccionada;
-  const marcasValidas = ["Asus", "Acer", "LG"];
-  do {
-    marcaSeleccionada = prompt("Elija una marca entre: Asus, Acer, LG");
-  } while (!validarOpcion(marcaSeleccionada, marcasValidas));
-  
-  let modeloSeleccionado;
-  const modelosValidos = ["Monitor Gamer Pro", "Monitor Gaming Master", "Monitor Nitro", "Monitor Predator", "Monitor UltraGear", "Monitor Gaming Pro"];
-  do {
-    modeloSeleccionado = prompt("Elija un modelo entre: Monitor Gamer Pro, Monitor Gaming Master, Monitor Nitro, Monitor Predator, Monitor UltraGear, Monitor Gaming Pro");
-  } while (!validarOpcion(modeloSeleccionado, modelosValidos));
-  
-  let tipoPanelSeleccionado;
-  const tiposPanelValidos = ["IPS", "TN", "VA"];
-  do {
-    tipoPanelSeleccionado = prompt("Elija un tipo de panel entre: IPS, TN, VA");
-  } while (!validarOpcion(tipoPanelSeleccionado, tiposPanelValidos));
-  
-  let frecuenciaSeleccionada;
-  const frecuenciasValidas = ["60Hz", "144Hz"];
-  do {
-    frecuenciaSeleccionada = prompt("Elija una frecuencia entre: 60Hz, 144Hz");
-  } while (!validarOpcion(frecuenciaSeleccionada, frecuenciasValidas));
-  
-  const costoMonitor = calcularCostoMonitor(marcaSeleccionada, modeloSeleccionado, tipoPanelSeleccionado, frecuenciaSeleccionada);
+} else if (marcaSeleccionada === "LG") {
+  if (modeloSeleccionado === "Monitor UltraGear") {
+    if (tipoPanelSeleccionado === "IPS") {
+      if (pulgadasSeleccionadas === "24 pulgadas") {
+        if (frecuenciaSeleccionada === "60Hz") {
+          costoMonitor = 280845;
+        } else if (frecuenciaSeleccionada === "144Hz") {
+          costoMonitor = 290000;
+        }
+      }
+    }
+  } else if (modeloSeleccionado === "Monitor Gaming Pro") {
+    if (tipoPanelSeleccionado === "TN") {
+      if (pulgadasSeleccionadas === "27 pulgadas") {
+        if (frecuenciaSeleccionada === "60Hz") {
+          costoMonitor = 210360;
+        } else if (frecuenciaSeleccionada === "144Hz") {
+          costoMonitor = 320980;
+        }
+      }
+    }
+  }
+}
 
-  if (costoMonitor > 0) {
-    alert(`El costo del monitor seleccionado es: $${costoMonitor}`);
-  } else {
-    alert("El monitor seleccionado no se encuentra disponible en nuestras opciones.");
-  }
+if (costoMonitor > 0) {
+  alert(`El costo del monitor seleccionado es: $${costoMonitor}`);
+} else {
+  alert("El monitor seleccionado no se encuentra disponible en nuestras opciones.");
+}
+
+
+
+
+
   
